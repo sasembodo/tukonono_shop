3958e294d39eaff671447d1ee213659e19097b33
sasembodo <sri.sembodo@ist.id>
2023-02-19 23:44:55 +0700

    first commit

---
+++ tukonono_shop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tukonono_shop
+//
+//  Created by user on 19/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
